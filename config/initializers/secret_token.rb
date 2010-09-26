d372f6e15d620e9336f446fac728d75611f9faa6
Orient <orient@orient-pc.(none)>
2010-09-26 16:11:52 +0800

    fisrt commit too

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Muxu::Application.config.secret_token = '4ee018137fc038b877edd7b67eb788ca3e447f8a4b678acdb6135f5397eb6fa1a756c57cf86a85ee38ad495262ed446cdad5779a969e0d390554ea48850e586d'
